72c3b632bb4255435c3b6d46261f62e71d1e988e
gochaism <gochaism@12ece14a-5d24-11de-b3c1-0fd540ce45e4>
2009-11-07 13:57:03 +0000

    Lua: add gui.parsecolor

--- trunk/snes9x-143/lua-engine.cpp
+++ trunk/snes9x-143/lua-engine.cpp
@@ -2426,6 +2426,18 @@ static int gui_getpixel(lua_State *L) {
 	return 3;
 }
 
+static int gui_parsecolor(lua_State *L)
+{
+	int r, g, b, a;
+	uint32 color = gui_getcolour(L,1);
+	LUA_DECOMPOSE_PIXEL(color, a, r, g, b);
+	lua_pushinteger(L, r);
+	lua_pushinteger(L, g);
+	lua_pushinteger(L, b);
+	lua_pushinteger(L, a);
+	return 4;
+}
+
 
 // gui.gdscreenshot()
 //
@@ -3831,6 +3843,7 @@ static const struct luaL_reg guilib[] = {
 	{"opacity", gui_setopacity},
 	{"transparency", gui_transparency},
 	{"popup", gui_popup},
+	{"parsecolor", gui_parsecolor},
 	{"gdscreenshot", gui_gdscreenshot},
 	{"gdoverlay", gui_gdoverlay},
 	{"getpixel", gui_getpixel},
