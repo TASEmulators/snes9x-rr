68420211d224d050259bf903eb0eb184d1a6f880
gocha <gochaism@gmail.com>
2020-01-19 13:52:13 +0900

    Upgrade from Visual Studio 2008 to Visual Studio 2017 and various portability improvements

--- version.h
+++ version.h
@@ -90,39 +90,5 @@
 
 
 #define SNES9X_NAME "Snes9x rerecording"
-
-#ifdef WIN32
-#include "svnrev.h"
-#else
-#ifdef SVN_REV
-#define SVN_REV_STR SVN_REV
-#else
-#define SVN_REV_STR ""
-#endif
-#endif
-
-#define SNES9X_FEATURE_STRING ""
-
-#ifdef DEBUG
-#define SNES9X_SUBVERSION_STRING " debug"
-#elif defined(PUBLIC_RELEASE)
-#define SNES9X_SUBVERSION_STRING ""
-#else
-#define SNES9X_SUBVERSION_STRING " svn" SVN_REV_STR
-#endif
-
-#if defined(_MSC_VER)
-#define SNES9X_COMPILER ""
-#define SNES9X_COMPILER_DETAIL " msvc " _Py_STRINGIZE(_MSC_VER)
-#define _Py_STRINGIZE(X) _Py_STRINGIZE1((X))
-#define _Py_STRINGIZE1(X) _Py_STRINGIZE2 ## X
-#define _Py_STRINGIZE2(X) #X
-//re: http://72.14.203.104/search?q=cache:HG-okth5NGkJ:mail.python.org/pipermail/python-checkins/2002-November/030704.html+_msc_ver+compiler+version+string&hl=en&gl=us&ct=clnk&cd=5
-#else
-// TODO: make for others compilers
-#define SNES9X_COMPILER ""
-#define SNES9X_COMPILER_DETAIL ""
-#endif
-
-#define SNES9X_VERSION_STRING "1.43 v18" SNES9X_SUBVERSION_STRING SNES9X_FEATURE_STRING SNES9X_COMPILER
+#define SNES9X_VERSION_STRING "1.43 v18"
 #define SNES9X_NAME_AND_VERSION SNES9X_NAME " " SNES9X_VERSION_STRING
