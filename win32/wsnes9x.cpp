0f754e94b4f6af452db76dea864868a9d4a2c42f
gocha <gochaism@gmail.com>
2026-08-24 14:47:03 +0900

    Fix heap buffer overflow in LoadExts()

--- win32/wsnes9x.cpp
+++ win32/wsnes9x.cpp
@@ -10653,7 +10653,7 @@ void LoadExts(void)
 				curr->compressed=true;
 			if(strlen(buffer)>1)
 			{
-				curr->extension=new char[strlen(buffer)-1];
+				curr->extension=new char[strlen(buffer)];
 				strncpy(curr->extension, buffer, strlen(buffer)-1);
 				curr->extension[strlen(buffer)-1]='\0';
 			}
